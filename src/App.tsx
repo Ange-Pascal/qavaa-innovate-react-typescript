dc0a18ce3eb5bc92d36f2b1ff3050622ff4d1373
Ange-Pascal <angeakoumoua@gmail.com>
2024-11-18 12:02:51 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
+
+  const [count, setCount] = useState(0)
+  function handlClick(){
+      setCount(count + 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header count={count} onClick= {handlClick} />
+      <Header count={count} onClick= {handlClick} />
+      <Header count={count} onClick= {handlClick} />
     </div>
   );
 }
---
+++ src/components/home/Instructor.tsx
@@ -0,0 +1,5 @@
+function Instructor(){
+
+}
+
+export default Instructor;
